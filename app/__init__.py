0b6d2039c0d4f857dcb17d3540bd77430900ce9d
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-10-18 15:07:41 -0500

    Added database configuration, updated init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from app.config import app, db
+from app.config import Config, Database
+
+config = Config()
+app = config.init_app()
+
+database = Database(app)
+db = database.get_db()
 
 from app.routes import routes
 app.register_blueprint(routes)
