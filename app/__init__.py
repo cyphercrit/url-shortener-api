44799cb6b17c68c124a0b088bb3a672250d13ea3
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-10-19 14:18:01 -0500

    Added azure logstream compatibility

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from urllib.parse import quote
 import os
+import logging # for azure logstream
 
 db = SQLAlchemy()
 
@@ -24,5 +25,13 @@ def create_app():
     # registers blueprints
     from app.routes import URLRoutes
     app.register_blueprint(URLRoutes().routes)
+
+    # configures logging for azure logstream
+    if not app.debug:
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger()
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+        logger.addHandler(handler)
     
     return app
