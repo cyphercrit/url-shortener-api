2c6c3b6a2f30e0e2170c5692b3971728d133f915
Caden Jurkovski <jurko036@umn.edu>
2024-10-20 00:08:24 -0500

    Changed ssl

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     db_password = quote(os.getenv("DB_PASSWORD")) # used for db passwords with special characters
     
     # configures the app
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DB_USER")}:{db_password}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}?sslmode=require'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DB_USER")}:{db_password}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # initializes the database
