9d63bc192502fe4e50f8c023a82cd889f49e9a61
Caden Jurkovski <jurko036@umn.edu>
2024-11-03 23:12:31 -0600

    Increased input URL maximum length

--- app/models.py
+++ app/models.py
@@ -2,5 +2,5 @@ from app import db
 
 class URL(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original_url = db.Column(db.String(500), nullable=False)
+    original_url = db.Column(db.String(2000), nullable=False)
     short_url = db.Column(db.String(10), unique=True, nullable=False)
